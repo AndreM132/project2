bc53dcdac03bd0225b00da485977276775dfdb8e
andrem132 <andrem132@hotmail.com>
2020-07-09 15:49:38 +0000

    up

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,6 @@
 
 ssh jenkins@swarm-master << EOF
 cd /home/andre_m132/project2
-docker stack deploy --compose-file docker-compose.yaml apps
+docker stack deploy --compose-file docker-compose.yaml stackapp
 
 EOF
