76d617c3616cc588ccfad8f6cad42039f2e096c6
andrem132 <andrem132@hotmail.com>
2020-07-09 15:22:22 +0000

    jenkins progression

--- script/build.sh
+++ script/build.sh
@@ -2,8 +2,6 @@
 
 
 
-version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 docker-compose build
 docker-compose push
 
