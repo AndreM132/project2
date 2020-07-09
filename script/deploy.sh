77a90a2d18bbccf2746da514e48ec53a3e1f0f4d
andrem132 <andrem132@hotmail.com>
2020-07-09 16:15:25 +0000

    update

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,6 @@
 ssh jenkins@swarm-master << EOF
 cd /home/andre_m132/project2
 . /home/andre_m132/.bashrc
-docker stack deploy --compose-file docker-compose.yaml stackapp
+env OUTFIT_DB_URI="${OUTFIT_DB_URI}" env NEW_SECRET_KEY="${NEW_SECRET_KEY}" docker stack deploy --compose-file docker-compose.yaml stackapp
 
 EOF
